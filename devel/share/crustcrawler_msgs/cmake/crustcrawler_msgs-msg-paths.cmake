aeb2a736bfb17c1852fa7ede5a1fff00fbd7628c
Vladimir Monakhov <vladimmo@ic-lc2r2n-014.uio.no>
2019-04-04 16:18:51 +0200

    Created package: in3140

--- devel/share/crustcrawler_msgs/cmake/crustcrawler_msgs-msg-paths.cmake
+++ devel/share/crustcrawler_msgs/cmake/crustcrawler_msgs-msg-paths.cmake
@@ -1,4 +1,4 @@
 # generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
 
-set(crustcrawler_msgs_MSG_INCLUDE_DIRS "/home/vladimmo/M-drive/project/src/crustcrawler_simulation/crustcrawler_msgs/msg")
+set(crustcrawler_msgs_MSG_INCLUDE_DIRS "/home/vladimmo/M-drive/IN3140_Project/src/crustcrawler_simulation/crustcrawler_msgs/msg")
 set(crustcrawler_msgs_MSG_DEPENDENCIES std_msgs;geometry_msgs)
