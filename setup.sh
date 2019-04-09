730b77040ffcf11ab4c1ce1fe23fd39549a6afed
vladim0105 <vladim0105@gmail.com>
2019-04-09 17:30:46 +0200

    Added ros sourcing in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
+source ~/.bashrc
 cd src
 git clone https://github.uio.no/INF3480/crustcrawler_simulation.git
 git clone https://github.uio.no/INF3480/crustcrawler_pen.git
