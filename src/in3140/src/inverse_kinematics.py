3046f7fafb084ffa6a7d5ea705b1b5f0325de50f
vladim0105 <vova010598>
2019-04-29 23:44:28 +0200

    Fixed minor bug in inverse kinematics

--- src/in3140/src/inverse_kinematics.py
+++ src/in3140/src/inverse_kinematics.py
@@ -3,6 +3,8 @@ import numpy as np
 L_1 = 100.9 / 10
 L_2 = 222.1 / 10
 L_3 = 136.2 / 10
+L_4 = 8.0
+t_4 = np.radians(180 - 45)
 
 
 def distToSphereCenter(center_coords, coords):
@@ -31,15 +33,21 @@ def getThetas(position):
     x = position[0]
     y = position[1]
     z = position[2]
+
+    t_1 = np.arctan2(y, x) + np.pi / 2
+
     r = np.sqrt(x ** 2 + y ** 2)
     s = z - L_1
-    c_squared = s ** 2 + r ** 2
-    D = (c_squared - L_2 ** 2 - L_3 ** 2) / (2 * L_2 * L_3)
-    t_1 = np.arctan2(y, x) + np.pi / 2
-    t_3 = np.arctan2(np.sqrt(1 - D ** 2), D)
-    t_2 = -np.arctan2(s, r) + np.arctan2(
-        np.cos(np.pi / 2 - t_3), (L_2 + L_3 * np.sin(np.pi / 2 - t_3))
-    )
 
+    c_3 = np.sqrt(L_3 ** 2 + L_4 ** 2 - 2 * L_3 * L_4 * np.cos(t_4))
+    cw_1 = (L_3 ** 2 - L_4 ** 2 + c_3 ** 2) / (2 * L_3 * c_3)
+    cw_2 = (L_2 ** 2 + c_3 ** 2 - (r ** 2 + s ** 2)) / (2 * L_2 * c_3)
+    w_1 = np.arctan2(np.sqrt(1 - cw_1 ** 2), cw_1)
+    t_3 = np.pi - w_1 - np.arctan2(np.sqrt(1 - cw_2 ** 2), cw_2)
+
+    ca_2 = (L_2 ** 2 - (c_3) ** 2 + (s ** 2 + r ** 2)) / (
+        2 * L_2 * np.sqrt(s ** 2 + r ** 2)
+    )
+    t_2 = np.pi / 2 - np.arctan2(s, r) - np.arctan2(np.sqrt(1 - ca_2 ** 2), ca_2)
     return [t_1, t_2, t_3]
 
